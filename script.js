f89b719cbb3ccea57ee892c820b67dcfedcfecb7
dlaszkowicz <d.laszkowicz@gmail.com>
2023-10-06 11:38:48 +0200

    Now every button is working one last thing to do is that pixels aren't stretching on whole board when more then 31x31

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let board = document.querySelector(".board");
 let size = document.querySelector("#size");
 let reset = document.querySelector("#reset");
 let rainbow = document.querySelector("#rainbow");
+let eraser = document.querySelector("#eraser");
+let black = document.querySelector("#black");
 function createBoard(size) {
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -21,11 +23,11 @@ pxs.forEach((px) => {
 });
 size.addEventListener("click", function () {
   let ask = prompt("Size:");
-  if (ask < 100 && ask > 2) {
+  if (ask < 31 && ask > 2) {
     clear();
     createBoard(ask);
   } else {
-    alert("Input value between 2 and 100");
+    alert("Input value between 2 and 30"); //TODO: pixels are not stretching on whole board when number of pixels is more then 31x31
   }
 });
 reset.addEventListener("click", function () {
@@ -35,5 +37,34 @@ function clear() {
   pxs.forEach((px) => {
     px.style.backgroundColor = "#f5f5dc";
   });
-  const random = Math.floor(Math.random() * 16777215).toString(16);
+}
+eraser.addEventListener("click", function () {
+  pxs.forEach((px) =>
+    px.addEventListener("mouseenter", function () {
+      px.style.backgroundColor = "#f5f5dc";
+    })
+  );
+});
+black.addEventListener("click", function () {
+  pxs.forEach((px) =>
+    px.addEventListener("mouseenter", function () {
+      px.style.backgroundColor = "black";
+    })
+  );
+});
+rainbow.addEventListener("click", function () {
+  pxs.forEach((px) =>
+    px.addEventListener("mouseenter", function () {
+      px.style.backgroundColor = randomColor();
+    })
+  );
+});
+function randomColor() {
+  return (
+    "#" +
+    Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, "0")
+      .toUpperCase()
+  );
 }
